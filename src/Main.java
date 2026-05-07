eb4841cb83d3909e9c15fcbe9d4835d2672b673e
ayushsingh46 <ayushsingh46@gmail.com>
2026-05-08 00:36:22 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("sum of these numbers is ");
+         int num1 = 5;
+         int num2 = 3;
+         int num3 = 8;
+         int sum = num1 + num2+ num3;
+
+            System.out.println(sum );
+        }
+    }
